c5ad2fdcfd5ee3720ed8abb7bff2e704ca3bc731
Jasmine Lin <house5324@gmail.com>
2018-01-10 14:03:25 +0800

    update3

--- js/all.js
+++ js/all.js
@@ -1,22 +1,22 @@
-const xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 xhr.open('get','https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97', true);
 xhr.send(null);
-let data ='';
-let district;
-const title = document.querySelector(".title");
-const content = document.querySelector(".content");
-const select = document.querySelector(".select-style");
+var data ='';
+var district;
+var title = document.querySelector(".title");
+var content = document.querySelector(".content");
+var select = document.querySelector(".select-style");
 
 xhr.onload = function(){
 	data = (JSON.parse(xhr.responseText)); 
     district = data.result.records; //資料在result.records裡
     //過濾乾淨的陣列到array裡
-	const areaList =[];
+	let areaList =[];
 	for(let i=0; i<district.length; i++){
 		areaList.push(district[i].Zone);
     }
     //再用forEach判斷陣列裡面所有的值是否有吻合
-	const area = [];
+	let area = [];
 	areaList.forEach(function(value){
 		if(area.indexOf(value) == -1){
 			area.push(value);
@@ -33,17 +33,17 @@ xhr.onload = function(){
     function update(e){
         let str = '';
         let tstr = '';
-        const main = e.target.value;
+        let main = e.target.value;
         for(let i=0; i<district.length; i++){
             if(main == district[i].Zone){
-                const img = district[i].Picture1;
-                const name = district[i].Name;
-                const lab = ' ';
-                const time = district[i].Opentime;
-                const address = district[i].Add;
-                const telnumber = district[i].Tel;
+                let img = district[i].Picture1;
+                let name = district[i].Name;
+                let lab = ' ';
+                let time = district[i].Opentime;
+                let address = district[i].Add;
+                let telnumber = district[i].Tel;
                 str += `<div><img class="contentImg" src="${img}"><p class="name">${name}</p><p class="icon1"><img src="images/icons_clock.png">${lab}${time}</p><p class="icon2"><img src="images/icons_pin.png">${lab}${address}</p><p class="icon3"><img src="images/icons_phone.png">${lab}${telnumber}</p></div>`;
-                tstr = `<ul>${main}</ul>`;
+                tstr = `<p>${main}</p>`;
             }
         }
         content.innerHTML = str;
